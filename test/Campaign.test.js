6d42782099e1897006c113edab5331e9a5234e00
joelethan <joelkatsme@gmail.com>
2022-03-28 14:06:49 +0300

    Refactor to use truffle for testing

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -1,64 +1,47 @@
-const assert = require("assert");
-const ganache = require("ganache-cli");
-const Web3 = require("web3");
+const CampaignFactory = artifacts.require("CampaignFactory");
+const Campaign = artifacts.require("Campaign");
 
-const web3 = new Web3(ganache.provider());
-
-const compiledCampaign = require("../ethereum/build/Campaign.json");
-
-const compiledFactory = require("../ethereum/build/CampaignFactory.json");
-
-let accounts;
 let campaignAddress;
 let campaign;
 let factory;
 
-beforeEach(async () => {
-  accounts = await web3.eth.getAccounts();
-  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
-    .deploy({ data: compiledFactory.bytecode })
-    .send({ from: accounts[0], gas: "1000000" });
-
-  await factory.methods
-    .createCampaign()
-    .send({ from: accounts[0], gas: "1000000" });
-  [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
-
-  campaign = await new web3.eth.Contract(
-    JSON.parse(compiledCampaign.interface),
-    campaignAddress
-  );
-});
-
-describe("Campaigns", () => {
-  it("deploys a factory and a campaign", () => {
-    assert.ok(factory.options.address);
-    assert.ok(campaign.options.address);
-  });
-
-  it("marks caller is the campaign manager", async () => {
-    const manager = await campaign.methods.manager().call();
-    assert.equal(manager, accounts[0]);
-  });
-
-  it("allows adding a board member", async () => {
-    await campaign.methods
-      .addMember(accounts[1])
-      .send({ from: accounts[0], gas: "1000000" });
-    const count = await campaign.methods.BOGcount().call();
-    assert.equal(count, 1);
-  });
-
-  it("allows a board member to contribute funds", async () => {
-    await campaign.methods
-      .addMember(accounts[1])
-      .send({ from: accounts[0], gas: "1000000" });
-    await campaign.methods.fundContract().send({
-      from: accounts[1],
-      value: web3.utils.toWei("10", "ether"),
-      gas: "1000000",
+contract("CampaignFactory", (accounts) => {
+  before(async () => {
+    await CampaignFactory.defaults({
+      from: accounts[2],
+      gas: 4612388,
     });
-    const balance = await campaign.methods.contractBalance().call();
-    assert.equal(balance, web3.utils.toWei("10", "ether"));
+    factory = await CampaignFactory.new();
+    await factory.createCampaign();
+    [campaignAddress] = await factory.getDeployedCampaigns();
+    campaign = await Campaign.at(campaignAddress);
+  });
+
+  describe("Campaign Deployment", () => {
+    it("contracts have been deployed", async () => {
+      assert.ok(factory.address);
+      assert.ok(campaign.address);
+    });
+
+    it("allows adding a Board Member", async () => {
+      await campaign.addMember(accounts[1], { from: accounts[2] });
+      const member = await campaign.boardOfGov(accounts[1]);
+      const count = await campaign.BOGcount();
+      assert.ok(member);
+      assert.equal(count.toNumber(), 1);
+    });
+
+    it("allows members to fund contract", async () => {
+      await campaign.fundContract({ from: accounts[1], value: 1000 });
+      const bal = await campaign.contractBalance();
+      assert.equal(bal.toNumber(), 1000);
+    });
+
+    it("allows manager to check a member's contribution", async () => {
+      const contribution = await campaign.getMemberContribution(accounts[1], {
+        from: accounts[2],
+      });
+      assert.equal(contribution.toNumber(), 1000);
+    });
   });
 });
